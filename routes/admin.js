3ab266814e3115e5e9572a0d1608ffbf12ea3d60
PrinceGupta123 <101912699+PrinceGupta123@users.noreply.github.com>
2023-11-26 22:39:23 +0530

    push

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,24 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 // import { verifyToken, isAdmin } from '../utils/jwtUtils.js';
-import { addProduct } from '../controller/adminController.js';
-import { validateProductSchema } from '../validations/productValidation.js';
-import multer from 'multer';
+import { addProduct } from "../controller/adminController.js";
+import { validateProductSchema } from "../validations/productValidation.js";
+import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads')
-    },
-    filename: function (req, file, cb) {
-      cb(null, `${Date.now()}- ${file.originalname}`)
-    }
-  })
-  const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}- ${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
 
-router.post("/add-product", [validateProductSchema, upload.array('pImage', 5)], addProduct);
+router.post(
+  "/add-product",
+  [validateProductSchema, upload.array("pImage", 5)],
+  addProduct
+);
+
 export default router;
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import {verifyOtp, generateOtp} from "../controller/authController.js";
-import { validateAuth, validateVerifyOtp } from '../validations/authValidation.js';
-import { verifyToken } from '../utils/jwtUtils.js';
+import { verifyOtp, generateOtp } from "../controller/authController.js";
+import {
+  validateAuth,
+  validateVerifyOtp,
+} from "../validations/authValidation.js";
+import { verifyToken } from "../utils/jwtUtils.js";
 router.post("/send-otp", validateAuth, generateOtp);
-router.post("/verifyOtp",validateVerifyOtp, verifyOtp);
-router.get("/test", verifyToken, (req, res)=>{
-    res.send("hello")
-})
+router.post("/verifyOtp", validateVerifyOtp, verifyOtp);
+// router.get("/test", verifyToken, (req, res)=>{
+//     res.send("hello")
+// })
 
 export default router;
